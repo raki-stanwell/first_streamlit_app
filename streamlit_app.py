bc073c9054d01e7b128719e4690e7c7708104f07
raki-stanwell <142479080+raki-stanwell@users.noreply.github.com>
2023-08-17 16:50:17 +1000

    Update streamlit_app.py
    Added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diners')
+
+streamlit.header('Breakfast List')
+streamlit.text('Idly')
+streamlit.text('2 Idly')
+streamlit.text('Dosa')
+
