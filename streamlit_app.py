5eac1d862872439980603759ac77ffa9b5b30670
raki-stanwell <142479080+raki-stanwell@users.noreply.github.com>
2023-08-22 19:10:07 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,15 +34,14 @@ def get_fruity_data(this_fruit_choice):
 streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not fruit_choice: 
-  streamlit.error("Please select] a fruit to get information.")
-else:
-  back_from_function = get_fruity_data(fruit_choice)
+  if not fruit_choice:
+    streamlit.error("Please select] a fruit to get information.")
+  else:
+    back_from_function = get_fruity_data(fruit_choice)
   streamlit.dataframe(back_from_function)
 
 except URLError as e:
   streamlit.error()
-
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
